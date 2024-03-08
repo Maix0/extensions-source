502d0508a0ef2dac00657586826e66c94fd0dc8c
Maieul BOYER <maieul.dev@familleboyer.net>
2024-03-08 17:43:11 +0100

    start3

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.android_sdk.accept_license = true;
+        };
+        sdk =
+          (pkgs.androidenv.composeAndroidPackages {
+            buildToolsVersions = ["30.0.3"];
+            platformVersions = ["34"];
+            abiVersions = ["arm64-v8a"];
+            systemImageTypes = [];
+            cmdLineToolsVersion = "8.0";
+          })
+          .androidsdk;
+      in {
+        devShell = pkgs.mkShell {
+          packages = [
+            sdk
+            pkgs.gradle
+          ];
+          ANDROID_SDK_ROOT = "${sdk}/libexec/android-sdk";
+        };
+      }
+    );
+}
